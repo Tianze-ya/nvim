0fa8a2b92af9d911951e85579d19d9b7fe9b51cf
Tianze-ya <1538058674@qq.com>
2024-12-21 17:59:48 +0800

    update

--- lua/config/plugins/theme.lua
+++ lua/config/plugins/theme.lua
@@ -1,6 +1,9 @@
 return {
-    'hardhackerlabs/theme-vim',
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
     config = function()
-      vim.cmd.colorscheme 'hardhacker'
+        vim.cmd [[colorscheme tokyonight]]
     end,
 }
